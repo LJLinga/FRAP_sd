871712f9a8f182905ed8553f828c43d745e12580
christianalderite <38313687+christianalderite@users.noreply.github.com>
2019-01-25 13:41:05 +0800

    d

--- run_AFEDPortal.bat
+++ run_AFEDPortal.bat
@@ -1,8 +1,6 @@
 @echo off 
     net start MySQL80
-    TIMEOUT 5
     net start Apache2.4
-    TIMEOUT 5
     cd "C:\Program Files (x86)\Google\Chrome\Application"
     start chrome.exe 127.0.0.1\FRAP_sd
     exit
