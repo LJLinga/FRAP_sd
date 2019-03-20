56bd6e5ec76219657378f987c61ba0ca2175f2a3
LJLinga <leonardo_linga@yahoo.com.ph>
2019-03-20 16:20:20 +0800

    Added User and Permissions page
    Contains the changes the roles of the members (EDMS, CMS, FRAP)

--- SYS_ADMIN_UsersPermissions.php
+++ SYS_ADMIN_UsersPermissions.php
@@ -7,12 +7,12 @@
  */
 
 
-include_once('GLOBAL_CLASS_CRUD.php');
-$crud = new GLOBAL_CLASS_CRUD();
-require_once('mysql_connect_FA.php');
+//include_once('GLOBAL_CLASS_CRUD.php');
+//$crud = new GLOBAL_CLASS_CRUD();
+//require_once('mysql_connect_FA.php');
 session_start();
-include('GLOBAL_USER_TYPE_CHECKING.php');
-include('GLOBAL_SYS_ADMIN_CHECKING.php');
+//include('GLOBAL_USER_TYPE_CHECKING.php');
+//include('GLOBAL_SYS_ADMIN_CHECKING.php');
 
 //hardcoded value for userType, will add MYSQL verification
 $userId = $_SESSION['idnum'];
@@ -27,28 +27,62 @@ include('CMS_SIDEBAR.php')
 <div id="content-wrapper">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Lifetime Membership Document</h1>
-                <ol class="breadcrumb">
-                    <li>
-                        1.0 Membership
-                    </li>
-                    <li>
-                        1.0.1 Requirements
-                    </li>
-                    <li class="active">
-                        1.0.1.1 Beneficiaries
-                    </li>
-                </ol>
-            </div>
-            <div class="col-lg-12">
+            <div class="col-lg-7">
 
-                <!-- IMPORTANT always put the Main Directory /FRAP_sd when refferencing documents using ViewerJS. If you don't, it will fail. -->
-                <iframe src = "/ViewerJS/../FRAP_sd/Lifetime_Document/Lifetime_Membership_Application_Form.pdf" width='800' height='800' allowfullscreen webkitallowfullscreen></iframe>
+                <h3>User roles and permissions</h3></br>
+
+                <form id="form" name="form" method="POST" action="<?php $_SERVER["PHP_SELF"]?>">
+                    <table class="table table-bordered" align="center" id="dataTable">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th width="100px">EDMS</th>
+                            <th width="100px">CMS</th>
+                            <th width="100px">Loans</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php for($i=0; $i<5; $i++){ ?>
+                            <tr>
+                                <td>Juan Dela Cruz</td>
+                                <td>
+                                    <select class="form-control">
+                                        <option value="">Reader</option>
+                                        <option value="">Technical Panel</option>
+                                        <option value="">Negotiation Head</option>
+                                        <option value="">Executive Board</option>
+                                        <option value="">President</option>
+                                        <option value="">Transcriber</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select class="form-control">
+                                        <option value="">Reader</option>
+                                        <option value="">Reviewer</option>
+                                        <option value="">Publisher</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select class="form-control">
+                                        <option value="">Member</option>
+                                        <option value="">Office Assistant</option>
+                                        <option value="">President</option>
+                                        <option value="">Treasurer</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                </form>
             </div>
         </div>
     </div>
 </div>
-
+<script>
+    $(document).ready(function(){
+        $('#dataTable').DataTable();
+    });
+</script>
 
 <?php include('GLOBAL_FOOTER.php'); ?>
