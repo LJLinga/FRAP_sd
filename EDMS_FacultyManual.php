eb12902c04f69a4f9afd19cfc41824e6aa0f4c6f
christianalderite <38313687+christianalderite@users.noreply.github.com>
2019-03-25 14:47:56 +0800

    PUSH

--- EDMS_FacultyManual.php
+++ EDMS_FacultyManual.php
@@ -36,25 +36,55 @@ $edmsRole = $_SESSION['EDMS_ROLE'];
         </div>
         <div class="row">
             <div class="col-lg-8">
-                <div class="card" style="margin-bottom: 1rem;">
-                    <div class="card-header">
-                        Manual Title
+            <?php
+                $rows = $crud->getData("SELECT v.sectionNo, v.title, v.content FROM facultyassocnew.published_versions pub
+                JOIN section_versions v ON pub.timeCreated = v.timeCreated
+                JOIN faculty_manual m ON m.id = pub.manualId
+				WHERE m.id = (SELECT MAX(m2.id) FROM faculty_manual m2 )
+				ORDER BY v.sectionNo; ");
+
+                $count = 0;
+                if(!empty($rows)){
+                    foreach((array) $rows as $key => $row){
+                        $count++;
+            ?>
+                <div class="panel panel-default" style="margin-bottom: 1rem;">
+                    <div class="panel-heading">
+                        <a class="btn btn-link" data-toggle="collapse" data-target="#collapse<?php echo $count;?>" aria-expanded="false" aria-controls="collapse<?php echo $count;?>">
+                            <div class="panel-title">Section <?php echo $row['sectionNo'].' - '.$row['title'];?></div>
+                        </a>
                     </div>
-                    <div class="card-body" >
-                        Manual Sections
-                        <div class="card">
+                    <div id="collapse<?php echo $count;?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                        <div class="panel-body" >
+                            <?php echo $row['content']; ?>
                         </div>
                     </div>
                 </div>
+            <?php }}?>
             </div>
-            <div class="col-lg-4">
-                <div class="card" style="margin-bottom: 1rem;">
-                    <div class="card-header"><i class="fa fa-archive fa-lg"></i> Old Editions</div>
-                    <div class="card-body">
-
+            <div class="col-lg-4" >
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        Published Manual Editions
+                    </div>
+                    <div class="panel-body">
+                        <?php
+                        $rows = $crud->getData("SELECT id, year, title, timePublished, publishedById 
+                                        FROM facultyassocnew.faculty_manual ORDER BY id DESC;");
+                        if(!empty($rows)){
+                            foreach((array)$rows AS $key => $row){
+                                echo '<div class="card" style="position: relative;">';
+                                echo '<div class="card-body">';
+                                echo $row['title'].' ('.$row['year'].')<br>';
+                                echo '<a href="EDMS_PublishSections.php?id='.$row['id'].'" target="_blank" class="btn btn-primary btn-sm" style="position: absolute; right: 2rem; top: 0.5rem;"><i class="fa fa-print"></i></a>';
+                                echo '</div></div>';
+                            }
+                        }else{
+                            echo 'You have no published manuals editions.';
+                        }
+                        ?>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
