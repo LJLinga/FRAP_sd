4b2b7d4da58904188ebdc4dff0605e6cb33d858a
Samuel-Gabrielle-Malijan <samgabriel_malijan@yahoo.com>
2019-08-12 22:13:26 +0800

    HA Benefit

--- fetch_header_notifs.php
+++ fetch_header_notifs.php
@@ -19,31 +19,78 @@ if(isset($_POST['idnum'])){
     if(mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_array($result))
         {
-            //if statements here, to know if they are
-            if($row['SERVICE_ID'] == 4 && $row['MEMBER_ID'] != $_SESSION['idnum'] && $row['TXN_TYPE'] == 1){
-                $outputs .= '
-                  <li>
-                  <span class="label label-info">Loans</span>
-                    <a href="ADMIN%20FALP%20applications.php">'."New FALP Application!".' </a>
-                    '.date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"])).'
-                  </li><br>
-                  ';
+            //4 - FAP Application 2 - HA 1 - Membership
+            //
+            if($row['SERVICE_ID'] == 4 && $row['MEMBER_ID'] != $_SESSION['idnum'] && $row['TXN_TYPE'] == 1){ // basically this one
+
+//                $outputs .= '
+//                    <a href="ADMIN%20FALP%20applications.php">
+//                  <li>
+//                  <span class="label label-info">Loans</span>
+//                    '."New FAP Application!".' </a>
+//                    '.date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"])).'
+//                  </li><br>
+//                  ';
+
+                $outputs .= '<a href="ADMIN%20FALP%20applications.php">';
+                $outputs .= '<li>';
+                $outputs .= '<span class="label label-info">Loans</span>';
+                $outputs .= '        New FAP Application!     ';
+                $outputs .=  date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"]));
+                $outputs .= '</li>';
+                $outputs .= '</a>';
+
+            }else  if($row['SERVICE_ID'] == 1 && $row['MEMBER_ID'] != $_SESSION['idnum'] && $row['TXN_TYPE'] == 1){
+//                $outputs .= '
+//                  <li>
+//                  <span class="label label-info">Health Aid</span>
+//                    <a href="ADMIN%20HEALTHAID%20applications.php">'."New Health Aid Application!".' </a>
+//                    '.date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"])).'
+//                  </li><br>
+//                  ';
+
+                $outputs .= '<a href="ADMIN%20MEMBERSHIP%20applications.php">';
+                $outputs .= '<li>';
+                $outputs .= '<span class="label label-info">Loans</span>';
+                $outputs .= '        New Membership Application!     ';
+                $outputs .=  date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"]));
+                $outputs .= '</li>';
+                $outputs .= '</a>';
+
             }else  if($row['SERVICE_ID'] == 2 && $row['MEMBER_ID'] != $_SESSION['idnum'] && $row['TXN_TYPE'] == 1){
-                $outputs .= '
-                  <li>
-                  <span class="label label-info">Health Aid</span>
-                    <a href="ADMIN%20HEALTHAID%20applications.php">'."New Health Aid Application!".' </a>
-                    '.date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"])).'
-                  </li><br>
-                  ';
+//                $outputs .= '
+//                  <li>
+//                  <span class="label label-info">Health Aid</span>
+//                    <a href="ADMIN%20HEALTHAID%20applications.php">'."New Health Aid Application!".' </a>
+//                    '.date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"])).'
+//                  </li><br>
+//                  ';
+
+                $outputs .= '<a href="ADMIN%20HEALTHAID%20applications.php">';
+                $outputs .= '<li>';
+                $outputs .= '<span class="label label-info">Loans</span>';
+                $outputs .= '        New Health Aid Application!     ';
+                $outputs .=  date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"]));
+                $outputs .= '</li>';
+                $outputs .= '</a>';
+
             }else if($row['SERVICE_ID'] == 4 && $row['MEMBER_ID'] != $_SESSION['idnum'] && $row['TXN_TYPE'] == 2){
-                $outputs .= '
-                  <li>
-                  <span class="label label-info">Loans</span>
-                    <a href="MEMBER%20FALP%20summary.php">'."Sucessfully Deducted".' </a>
-                    '.date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"])).'
-                  </li><br>
-                  ';
+//                $outputs .= '
+//                  <li>
+//                  <span class="label label-info">Loans</span>
+//                    <a href="MEMBER%20FALP%20summary.php">'."Sucessfully Deducted".' </a>
+//                    '.date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"])).'
+//                  </li><br>
+//                  ';
+
+                $outputs .= '<a href="ADMIN%20HEALTHAID%20applications.php">';
+                $outputs .= '<li>';
+                $outputs .= '<span class="label label-info">Loans</span>';
+                $outputs .= '        Sucessfully Deducted for Loans.     ';
+                $outputs .=  date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"]));
+                $outputs .= '</li>';
+                $outputs .= '</a>';
+
             }else if($row['SERVICE_ID'] == 2 && $row['MEMBER_ID'] != $_SESSION['idnum'] && $row['TXN_TYPE'] == 2){
                 $outputs .= '
                   <li>
@@ -52,6 +99,23 @@ if(isset($_POST['idnum'])){
                     '.date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"])).'
                   </li><br>
                   ';
+            }else if($row['SERVICE_ID'] == 4 && $row['MEMBER_ID'] != $_SESSION['idnum'] && $row['TXN_TYPE'] == 2){
+//                $outputs .= '
+//                  <li>
+//                  <span class="label label-info">Loans</span>
+//                    <a href="MEMBER%20FALP%20summary.php">'."Sucessfully Deducted".' </a>
+//                    '.date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"])).'
+//                  </li><br>
+//                  ';
+
+                $outputs .= '<a href="ADMIN%20HEALTHAID%20applications.php">';
+                $outputs .= '<li>';
+                $outputs .= '<span class="label label-info">Loans</span>';
+                $outputs .= '        New FAP Application!     ';
+                $outputs .=  date("F j, Y g:i:s A ", strtotime($row["TXN_DATE"]));
+                $outputs .= '</li>';
+                $outputs .= '</a>';
+
             }else if($row['SERVICE_ID'] == 4 ){
                 $outputs .= '
                   <li>
